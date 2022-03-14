0ac689fcb97e90a3ac9fbd99bf30146c0d8d7793
SlamperBOOM <slavashink@gmail.com>
2022-03-14 23:13:00 +0700

    Delete calculator directory

--- calculator/Operation.java
+++
@@ -1,8 +0,0 @@
-package calculator;
-import exceptions.CalculatorException;
-
-import java.util.List;
-
-public interface Operation {
-    List<Double> calc(List<String> arguments, List<Double> stack, Calculator calculator) throws CalculatorException;
-}
